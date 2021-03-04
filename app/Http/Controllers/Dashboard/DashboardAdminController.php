534dc06dfee2258537d817e8dc1eba3d2b747756
mike-taylorprops <mike@taylorprops.com>
2021-03-04 14:58:12 -0500

    daily changes

--- app/Http/Controllers/Dashboard/DashboardAdminController.php
+++ app/Http/Controllers/Dashboard/DashboardAdminController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Artisan;
 
 class DashboardAdminController extends Controller
 {
     public function dashboard_admin(Request $request) {
+
+        Artisan::call('doc_management:check_emailed_documents');
+        dd(Artisan::output());
+
         return view('/dashboard/admin/dashboard');
     }
 }
