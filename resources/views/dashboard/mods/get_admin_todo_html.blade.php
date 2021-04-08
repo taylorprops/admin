69195c7dc732acafe0a8faa4be04dd20b12f6bcb
mike-taylorprops <mike@taylorprops.com>
2021-04-08 08:58:24 -0400

    daily changes

--- resources/views/dashboard/mods/get_admin_todo_html.blade.php
+++ resources/views/dashboard/mods/get_admin_todo_html.blade.php
@@ -40,10 +40,10 @@
         <div class="d-flex justify-content-between align-items-center bg-blue-light p-2 rounded">
 
             <div class="text-primary font-10">
-                <a href="/doc_management/document_review">Pending Earnest Checks</a>
+                <a href="/doc_management/balance_earnest">Pending Earnest Checks</a>
             </div>
             <div class="bg-orange font-11 text-white rounded p-2 w-15 text-center">
-                <a href="/doc_management/document_review" class="text-white">{{ $pending_earnest_count }}</a>
+                <a href="/doc_management/balance_earnest" class="text-white">{{ $pending_earnest_count }}</a>
             </div>
 
         </div>
@@ -55,10 +55,10 @@
         <div class="d-flex justify-content-between align-items-center bg-blue-light p-2 rounded">
 
             <div class="text-primary font-10">
-                <a href="/doc_management/document_review">Pending Commission Breakdowns</a>
+                <a href="/doc_management/commission">Pending Commission Breakdowns</a>
             </div>
             <div class="bg-orange font-11 text-white rounded p-2 w-15 text-center">
-                <a href="/doc_management/document_review" class="text-white">{{ $pending_commissions_count }}</a>
+                <a href="/doc_management/commission" class="text-white">{{ $pending_commissions_count }}</a>
             </div>
 
         </div>
