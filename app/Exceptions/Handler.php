b644a8cd9b465fad4eb131eaf9c704aab25ddf6c
mike-taylorprops <mike@taylorprops.com>
2021-04-21 07:54:27 -0400

    daily changes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -41,7 +41,7 @@ class Handler extends ExceptionHandler
         });
     }
 
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         if ($this -> shouldReport($exception)) {
             $airbrakeNotifier = \App::make('Airbrake\Notifier');
