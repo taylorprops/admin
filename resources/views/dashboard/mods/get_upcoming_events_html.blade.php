feba9f31d1a5dd236b3806d32c0025c62a0221c3
mike-taylorprops <mike@taylorprops.com>
2021-04-29 12:17:19 -0400

    daily changes

--- resources/views/dashboard/mods/get_upcoming_events_html.blade.php
+++ resources/views/dashboard/mods/get_upcoming_events_html.blade.php
@@ -1,61 +1,67 @@
 @if(count($upcoming_events) > 0)
 
-<div class="list-group">
+    <div class="list-group">
 
-    @foreach($upcoming_events as $event)
+        @foreach($upcoming_events as $event)
 
-        @php
-        $event_time = null;
-        if($event -> event_type == 'reminder') {
-            $event_time = $event -> start_time;
-        }
+            @php
+            $event_time = null;
+            if($event -> event_type == 'reminder') {
+                $event_time = $event -> start_time;
+            }
 
 
-        if($event -> event_type == 'event') {
-            $icon = '<i class="fal fa-calendar text-primary fa-lg"></i>';
-        } else if($event -> event_type == 'reminder') {
-            $icon = '<i class="fal fa-clock text-gray fa-lg"></i>';
-        } else if($event -> event_type == 'task') {
-            $icon = '<i class="fal fa-tasks text-primary fa-lg"></i>';
-        }
+            if($event -> event_type == 'event') {
+                $icon = '<i class="fal fa-calendar text-primary fa-lg"></i>';
+            } else if($event -> event_type == 'reminder') {
+                $icon = '<i class="fal fa-clock text-gray fa-lg"></i>';
+            } else if($event -> event_type == 'task') {
+                $icon = '<i class="fal fa-tasks text-primary fa-lg"></i>';
+            }
 
-        $link = null;
-        $transction_type = $event -> transaction_type;
-        if($transction_type) {
-            $id = $transction_type == 'listing' ? $event -> Listing_ID : $event -> Contract_ID;
-            $link = '/agents/doc_management/transactions/transaction_details/'.$id.'/'.$transction_type;
-        }
-        @endphp
+            $link = null;
+            $transction_type = $event -> transaction_type;
+            if($transction_type) {
+                $id = $transction_type == 'listing' ? $event -> Listing_ID : $event -> Contract_ID;
+                $link = '/agents/doc_management/transactions/transaction_details/'.$id.'/'.$transction_type;
+            }
+            @endphp
 
-        <div class="list-group-item">
+            <div class="list-group-item">
 
-            <div class="d-flex justify-content-start align-items-center">
+                <div class="d-flex justify-content-start align-items-center">
 
-                <div class="mr-4">
-                    {!! $icon !!}
-                </div>
+                    <div class="mr-4">
+                        {!! $icon !!}
+                    </div>
 
-                <div class="font-9 mr-4">
-                    {{ date('D - M jS', strtotime($event -> start_date)) }}
-                    @if($event_time)
-                        <br>
-                        <span class="font-8">{{ date('g:ia', strtotime($event -> start_time)) }}</span>
-                    @endif
-                </div>
+                    <div class="font-9 mr-4">
+                        {{ date('D - M jS', strtotime($event -> start_date)) }}
+                        @if($event_time)
+                            <br>
+                            <span class="font-8">{{ date('g:ia', strtotime($event -> start_time)) }}</span>
+                        @endif
+                    </div>
 
-                <div>
-                    {{ $event -> event_title }}
-                    @if($link)
-                    <div class="font-8"><a href="{{ $link }}" target="_blank">View Transaction</a></div>
-                    @endif
+                    <div>
+                        {{ $event -> event_title }}
+                        @if($link)
+                        <div class="font-8"><a href="{{ $link }}" target="_blank">View Transaction</a></div>
+                        @endif
+                    </div>
+
                 </div>
 
             </div>
 
-        </div>
+        @endforeach
 
-    @endforeach
+    </div>
 
-</div>
+@else
+
+    <div class="text-gray font-10 text-center p-2">
+        <i class="fal fa-check mr-2"></i> No Upcoming Events
+    </div>
 
 @endif
