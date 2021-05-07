6131c31a944687690a19de3b0f6d659a1c183ebd
mike-taylorprops <mike@taylorprops.com>
2021-05-07 09:26:30 -0400

    daily changes

--- resources/js/agents/doc_management/transactions/details/details_tabs/documents.js
+++ resources/js/agents/doc_management/transactions/details/details_tabs/documents.js
@@ -78,10 +78,10 @@ if (document.URL.match(/transaction_details/)) {
         $(document).on('change', '.check-document', show_bulk_options);
 
         $('.sortable-documents').on('hidden.bs.collapse', function () {
-            $('.folder-collapse[href="'+$(this).prop('id')+'"]').find('i').removeClass('fa-angle-down').addClass('fa-angle-right');
+            $('.folder-collapse[href="#'+$(this).prop('id')+'"]').find('i.fa-angle-down').removeClass('fa-angle-down').addClass('fa-angle-right');
         });
         $('.sortable-documents').on('shown.bs.collapse', function () {
-            $('.folder-collapse[href="'+$(this).prop('id')+'"]').find('i').addClass('fa-angle-down').removeClass('fa-angle-right');
+            $('.folder-collapse[href="#'+$(this).prop('id')+'"]').find('i.fa-angle-right').addClass('fa-angle-down').removeClass('fa-angle-right');
         });
 
     });
